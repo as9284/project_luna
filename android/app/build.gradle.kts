f88dbecc24f8d6498a1a0ec00d6e410853afe235
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-04-17 12:47:20 +0300

    Update android info

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.project_luna"
+    namespace = "com.as.project_luna"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
